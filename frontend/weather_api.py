d22b2e910d06494a8a2618824f7b935f9caa8fae
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2023-09-25 14:39:02 +0530

    created dataclass for parsing 3 days weather data from JSON

--- frontend/weather_api.py
+++ frontend/weather_api.py
@@ -1,10 +1,22 @@
 import os
 import requests
-from datetime import datetime
-from dotenv import load_dotenv
+import pandas as pd
+from datetime import ( datetime )
+from dotenv import ( load_dotenv )
+from dataclasses import ( dataclass )
+from time import ( strftime, localtime )
+
+@dataclass
+class WeatherData:
+    date: datetime
+    temp_c: float
+    feels_like: float
+    weather_condition: str
+    wind_kph: float
+    wind_dir: str
 
 class WeatherApi():
-    
+
     def __init__(self, place):
         load_dotenv()
         self.query = place
@@ -29,7 +41,7 @@ class WeatherApi():
         """
         url = f"{self.api_url}current.json?{self.query_key}&dt={self.dt}"
         resp = requests.get(url=url)
-        
+
         # Return the JSON data if status code is 200
         if resp.status_code == 200:
             data = resp.json()
@@ -43,17 +55,35 @@ class WeatherApi():
             return json_data
         else:
             return 'Uh ooh, something went wrong while retriving data. Please check your input'
-    
-    def get_seven_days_weather(self):
+
+    def get_three_days_weather(self):
         """
-        Method to get next 7 days forecast data
+        Method to get next 3 days forecast data
         """
         url = f"{self.api_url}forecast.json?{self.query_key}&days=3"
         resp = requests.get(url)
-        
+
+        # weather data list
+        weather_data_list = []
+
         # return the JSON data if status code is 200
         if resp.status_code == 200:
-            data = resp.json()
-            return data['forecast']
+            data = resp.json()['forecast']['forecastday']
+            for forecast in data:
+                for hour_data in forecast['hour']:
+                    weather_data = WeatherData(
+                        # strftime('%Y-%m-%d %H:%M:%S', localtime(hour_data['time_epoch']))
+                        date = strftime('%Y-%m-%d %H:%M:%S', localtime(hour_data['time_epoch'])),
+                        temp_c = hour_data['temp_c'],
+                        feels_like = hour_data['feelslike_c'],
+                        weather_condition = hour_data['condition']['text'],
+                        wind_kph = hour_data['wind_kph'],
+                        wind_dir = hour_data['wind_dir']
+                    )
+                    weather_data_list.append(weather_data)
+            weather_df = pd.DataFrame(
+                [weather_data.__dict__ for weather_data in weather_data_list]
+            )
+            return weather_df
         else:
             return 'Uh ooh, something went wrong while retriving data. Please check your input.'
